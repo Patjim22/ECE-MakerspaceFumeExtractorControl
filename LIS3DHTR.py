79125ac76977fcd48557f3869f871f902da68c8d
Ryan-Ficken <rcficken@ncsu.edu>
2023-02-20 11:03:49 -0500

    New and Improved!

--- LIS3DHTR.py
+++ LIS3DHTR.py
@@ -11,11 +11,13 @@ import time
 import RPi.GPIO as GPIO
  
 # Get I2C bus
-bus = smbus.SMBus(1)
- 
+buslist = [11,12,13,14,15,16,17,18]
+bus = [smbus.SMBus(buslist[0]),smbus.SMBus(buslist[1]),smbus.SMBus(buslist[2]),smbus.SMBus(buslist[3]),smbus.SMBus(buslist[4]),smbus.SMBus(buslist[5]),smbus.SMBus(buslist[6]),smbus.SMBus(buslist[7])]
+
 # I2C address of the device
 LIS3DHTR_DEFAULT_ADDRESS            = 0x19
 LIS3DHTR_SECOND_ADDRESS             = 0x18
+LIS3DHTR_SECOND_ENABLE              = 0
  
 # LIS3DHTR Register Map
 LIS3DHTR_REG_WHOAMI                 = 0x0F # Who Am I Register
@@ -76,167 +78,114 @@ LIS3DHTR_INT1_MOTION_DETECT         = 0x0A # 6-Direction Movement Recognition
  
  
 class LIS3DHTR():
-    def __init__ (self):
-        self.select_datarate()
-        self.select_data_config()
+    def __init__ (self,addressList,numSensors):
+        self.addressList = addressList
+        self.numSensors = numSensors
+        self.select_datarate(self)
+        self.select_data_config(self)
  
     def select_datarate(self):
         """Select the data rate of the accelerometer from the given provided values"""
         DATARATE_CONFIG = (LIS3DHTR_ACCL_DR_10 | LIS3DHTR_ACCL_XAXIS | LIS3DHTR_ACCL_YAXIS | LIS3DHTR_ACCL_ZAXIS)
-        bus.write_byte_data(LIS3DHTR_DEFAULT_ADDRESS, LIS3DHTR_REG_CTRL1, DATARATE_CONFIG)
-        bus.write_byte_data(LIS3DHTR_SECOND_ADDRESS, LIS3DHTR_REG_CTRL1, DATARATE_CONFIG)
+        for i in range(self.numSensors):
+            bus.write_byte_data(self.addressList[i], LIS3DHTR_REG_CTRL1, DATARATE_CONFIG)
+            
  
     def select_data_config(self):
         """Select the data configuration of the accelerometer from the given provided values"""
         DATA_CONFIG = (LIS3DHTR_ACCL_RANGE_2G | LIS3DHTR_BDU_CONT | LIS3DHTR_HR_DS)
-        bus.write_byte_data(LIS3DHTR_DEFAULT_ADDRESS, LIS3DHTR_REG_CTRL4, DATA_CONFIG)
-        bus.write_byte_data(LIS3DHTR_SECOND_ADDRESS, LIS3DHTR_REG_CTRL4, DATA_CONFIG)
+        for i in range(self.numSensors):
+            bus.write_byte_data(self.addressList[i], LIS3DHTR_REG_CTRL4, DATA_CONFIG)
+
  
-    def read_accl1(self):
+    def read_accl(self):
         """Read data back from LIS3DHTR_REG_OUT_X_L(0x28), 2 bytes
         X-Axis Accl LSB, X-Axis Accl MSB"""
-        data0 = bus.read_byte_data(LIS3DHTR_DEFAULT_ADDRESS, LIS3DHTR_REG_OUT_X_L)
-        data1 = bus.read_byte_data(LIS3DHTR_DEFAULT_ADDRESS, LIS3DHTR_REG_OUT_X_H)
+        xAccl = 0
+        yAccl = 0
+        zAccl = 0
+        for i in range(self.numSensors):
+            data0 = bus.read_byte_data(self.addressList[i], LIS3DHTR_REG_OUT_X_L)
+            data1 = bus.read_byte_data(self.addressList[i], LIS3DHTR_REG_OUT_X_H)
  
-        xAccl = data1 * 256 + data0
-        if xAccl > 32767 :
-            xAccl -= 65536
-        xAccl /= 16000
-        """Read data back from LIS3DHTR_REG_OUT_Y_L(0x2A), 2 bytes
-        Y-Axis Accl LSB, Y-Axis Accl MSB"""
-        data0 = bus.read_byte_data(LIS3DHTR_DEFAULT_ADDRESS, LIS3DHTR_REG_OUT_Y_L)
-        data1 = bus.read_byte_data(LIS3DHTR_DEFAULT_ADDRESS, LIS3DHTR_REG_OUT_Y_H)
+            xAccl[i] = data1 * 256 + data0
+            if xAccl[i] > 32767 :
+                xAccl[i] -= 65536
+            xAccl[i] /= 16000
+            """Read data back from LIS3DHTR_REG_OUT_Y_L(0x2A), 2 bytes
+            Y-Axis Accl LSB, Y-Axis Accl MSB"""
+            data0 = bus.read_byte_data(self.addressList[i], LIS3DHTR_REG_OUT_Y_L)
+            data1 = bus.read_byte_data(self.addressList[i], LIS3DHTR_REG_OUT_Y_H)
  
-        yAccl = data1 * 256 + data0
-        if yAccl > 32767 :
-            yAccl -= 65536
-        yAccl /= 16000
-        """Read data back from LIS3DHTR_REG_OUT_Z_L(0x2C), 2 bytes
-        Z-Axis Accl LSB, Z-Axis Accl MSB"""
-        data0 = bus.read_byte_data(LIS3DHTR_DEFAULT_ADDRESS, LIS3DHTR_REG_OUT_Z_L)
-        data1 = bus.read_byte_data(LIS3DHTR_DEFAULT_ADDRESS, LIS3DHTR_REG_OUT_Z_H)
+            yAccl[i] = data1 * 256 + data0
+            if yAccl[i] > 32767 :
+                yAccl[i] -= 65536
+            yAccl[i] /= 16000
+            """Read data back from LIS3DHTR_REG_OUT_Z_L(0x2C), 2 bytes
+            Z-Axis Accl LSB, Z-Axis Accl MSB"""
+            data0 = bus.read_byte_data(self.addressList[i], LIS3DHTR_REG_OUT_Z_L)
+            data1 = bus.read_byte_data(self.addressList[i], LIS3DHTR_REG_OUT_Z_H)
  
-        zAccl = data1 * 256 + data0
-        if zAccl > 32767 :
-            zAccl -= 65536
-        zAccl /= 16000
+            zAccl[i] = data1 * 256 + data0
+            if zAccl[i] > 32767 :
+                zAccl[i] -= 65536
+            zAccl[i] /= 16000
         return {'x' : xAccl, 'y' : yAccl, 'z' : zAccl}
-
-    def read_accl2(self):
-        """Read data back from LIS3DHTR_REG_OUT_X_L(0x28), 2 bytes
-        X-Axis Accl LSB, X-Axis Accl MSB"""
-        data0 = bus.read_byte_data(LIS3DHTR_SECOND_ADDRESS, LIS3DHTR_REG_OUT_X_L)
-        data1 = bus.read_byte_data(LIS3DHTR_SECOND_ADDRESS, LIS3DHTR_REG_OUT_X_H)
-
-        xAccl = data1 * 256 + data0
-        if xAccl > 32767 :
-            xAccl -= 65536
-        xAccl /= 16000
-        """Read data back from LIS3DHTR_REG_OUT_Y_L(0x2A), 2 bytes
-        Y-Axis Accl LSB, Y-Axis Accl MSB"""
-        data0 = bus.read_byte_data(LIS3DHTR_SECOND_ADDRESS, LIS3DHTR_REG_OUT_Y_L)
-        data1 = bus.read_byte_data(LIS3DHTR_SECOND_ADDRESS, LIS3DHTR_REG_OUT_Y_H)
-
-        yAccl = data1 * 256 + data0
-        if yAccl > 32767 :
-            yAccl -= 65536
-        yAccl /= 16000
-        """Read data back from LIS3DHTR_REG_OUT_Z_L(0x2C), 2 bytes
-        Z-Axis Accl LSB, Z-Axis Accl MSB"""
-        data0 = bus.read_byte_data(LIS3DHTR_SECOND_ADDRESS, LIS3DHTR_REG_OUT_Z_L)
-        data1 = bus.read_byte_data(LIS3DHTR_SECOND_ADDRESS, LIS3DHTR_REG_OUT_Z_H)
-
-        zAccl = data1 * 256 + data0
-        if zAccl > 32767 :
-            zAccl -= 65536
-        zAccl /= 16000
-        return {'x' : xAccl, 'y' : yAccl, 'z' : zAccl}
- 
-    def enable_INT1(self):
-        """Enables the motion interrupt for reading motion"""
-        CTRL_REG3_CONF = 0x40
-        INT1_CONFIG = 0x0A  # (LIS3DHTR_INT1_MOTION_DETECT)
-        print(bin(INT1_CONFIG))
-        CTRL_REG5_CONF = 0x08
-        INT1_THRESH = 0x04
-        INT1_DURATION = 0x0A
-        bus.write_byte_data(LIS3DHTR_DEFAULT_ADDRESS,LIS3DHTR_REG_CTRL3,CTRL_REG3_CONF)
-        bus.write_byte_data(LIS3DHTR_DEFAULT_ADDRESS,LIS3DHTR_REG_CTRL5,CTRL_REG5_CONF)
-        bus.write_byte_data(LIS3DHTR_DEFAULT_ADDRESS,LIS3DHTR_INT1_THS,INT1_THRESH)
-        bus.write_byte_data(LIS3DHTR_DEFAULT_ADDRESS,LIS3DHTR_INT1_DURATION,INT1_DURATION)
-        bus.write_byte_data(LIS3DHTR_DEFAULT_ADDRESS,LIS3DHTR_INT1_CFG,INT1_CONFIG)
-
-
-    def read_INT1(self):
-        """Reads the SRC Register"""
-        data = bus.read_byte_data(LIS3DHTR_DEFAULT_ADDRESS, LIS3DHTR_INT1_SRC)
-        return data
  
 from LIS3DHTR import LIS3DHTR
-lis3dhtr = LIS3DHTR()
- 
-lis3dhtr.select_datarate()
-lis3dhtr.select_data_config()
-lis3dhtr.enable_INT1()
-print(bin(lis3dhtr.read_INT1()))
+c = 0
+for i in range(0,len(bus)):
+    for j in range(2,120):
+        try:
+            addressList[c] = bus[i].read_byte_data(j)
+            c = c + 1
+        except:
+            addressList[c] = 0
+    numAddresses[i] = c
+    c = 0
+    lis3dhtr[i] = LIS3DHTR(addressList,numAddresses)
+
 time.sleep(1)
-accl_old1 = lis3dhtr.read_accl1()
-accl_old2 = lis3dhtr.read_accl2()
-count1 = 0
-count2 = 0
-lowcount1 = 0
-lowcount2 = 0
+for i in range(0,len(bus)):
+    accl_old[i] = lis3dhtr[i].read_accl()
+    for j in range(numAddresses[i]):
+        count[i][j] = 0
+        lowcount[i][j] = 0
+        fanOn[i][j] = 0
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(23,GPIO.IN)
 GPIO.setup(18,GPIO.OUT)
 GPIO.output(18,GPIO.LOW)
 while True:
-    #print(bin(lis3dhtr.read_INT1()))
-    accl1 = lis3dhtr.read_accl1()
-    accl2 = lis3dhtr.read_accl2()
-    print(abs(accl_old1['z'] - accl1['z']),abs(accl_old1['y'] - accl1['y']))
-    if abs(accl_old1['x'] - accl1['x']) > .10 or abs(accl_old1['y'] - accl1['y'])>.10:
-       count1 += 1
-       lowcount1 = 0
-       lowcount2 = 0
-       if count1 > 2 : 
-           print("TURN FAN ON!")
-           count1 = 0
-           GPIO.output(18,GPIO.HIGH)
-           time.sleep(25)
+    for i in range(0,len(bus)):
+        accl = lis3dhtr[i].read_accl()
+        for j in range(numAddresses[i]):
+            if abs(accl_old['x'][i][j] - accl['x'][j]) > .15 or abs(accl_old['y'][i][j] - accl['y'][j]) > .15 or abs(accl_old['z'][i][j] - accl['z'][j]) > .15:
+                count[i][j] += 1
+                lowcount[i][j] = 0
+                if count[i][j] > 2:
+                    sensorOn[i][j] = 1
+                    count[i][j] = 0
+            else:
+                count[i][j] = 0
+                lowcount[i][j] += 1
+                if lowcount[i][j] > 4:
+                    sensorOn[i][j] = 0
+                    lowcount[i][j] = 0
+        accl_old[i] = accl
+    if any(sensorOn):
+        print("TURN FAN ON!")
+        for i in range(0,len(bus)):
+            for j  in range(numAddresses[i]):
+                sensorOn[i][j] = 0
+        GPIO.output(18,GPIO.HIGH)
+        time.sleep(25)
     else:
-       count1 = 0
-       lowcount1 += 1
-    print(count1)
-    print(abs(accl_old2['x'] - accl2['x']),abs(accl_old2['y'] - accl2['y']))
-    if abs(accl_old2['x'] - accl2['x']) > .10 or abs(accl_old2['y'] - accl2['y'])>.10:
-       count2 += 1
-       lowcount2 = 0
-       lowcount1 = 0
-       if count2 > 2 :
-           print("TURN FAN ON!")
-           count2 = 0
-           GPIO.output(18,GPIO.HIGH)
-           print("Sleeping")
-           time.sleep(25)
-           print("Sleeping")
-           
-    else:
-       count2 = 0
-       lowcount2 += 1
-
-    if lowcount2 > 4 or lowcount1 > 4:
-       print("Fan Turned Off")
-       GPIO.output(18,GPIO.LOW)
-       lowcount2 = 0
-       lowcount1 = 0
-
-    print(count2)
-    accl_old1 = accl1
-    accl_old2 = accl2
-    print("Acceleration in X-Axis : %f" %(accl2['x']))
-    print("Acceleration in Y-Axis : %f" %(accl2['y']))
-    print("Acceleration in Z-Axis : %f" %(accl2['z']))
-    print(" ************************************ ")
+        print("Fan Turned Off")
+        GPIO.output(18,GPIO.LOW)
+        for i in range(0,len(bus)):
+            for j  in range(numAddresses[i]):
+                lowcount[i][j] = 0
+    
     time.sleep(2)
 
