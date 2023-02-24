35c815289d2868ca4c6ff9b71587b59ef64ff501
Ryan-Ficken <rcficken@ncsu.edu>
2023-02-24 11:11:03 -0500

    why second run through

--- LIS3DHTR.py
+++ LIS3DHTR.py
@@ -90,7 +90,7 @@ class LIS3DHTR():
         DATARATE_CONFIG = (LIS3DHTR_ACCL_DR_10 | LIS3DHTR_ACCL_XAXIS | LIS3DHTR_ACCL_YAXIS | LIS3DHTR_ACCL_ZAXIS)
         for i in range(len(numSensors)):
             if addressList:
-                print(addressList,busnum,i)
+                print(addressList,busnum,i,len(numSensors))
                 bus[busnum].write_byte_data(addressList[i], LIS3DHTR_REG_CTRL1, DATARATE_CONFIG)
             
  
