fe0dc9b0486842475bbc3c5958d35b225c4f776b
Ryan-Ficken <rcficken@ncsu.edu>
2023-02-24 11:01:28 -0500

    Checking lists

--- LIS3DHTR.py
+++ LIS3DHTR.py
@@ -88,7 +88,8 @@ class LIS3DHTR():
         """Select the data rate of the accelerometer from the given provided values"""
         DATARATE_CONFIG = (LIS3DHTR_ACCL_DR_10 | LIS3DHTR_ACCL_XAXIS | LIS3DHTR_ACCL_YAXIS | LIS3DHTR_ACCL_ZAXIS)
         for i in range(len(numSensors)):
-            if any(addressList):
+            if addressList:
+                print(addressList)
                 bus.write_byte_data(addressList[i], LIS3DHTR_REG_CTRL1, DATARATE_CONFIG)
             
  
@@ -96,7 +97,7 @@ class LIS3DHTR():
         """Select the data configuration of the accelerometer from the given provided values"""
         DATA_CONFIG = (LIS3DHTR_ACCL_RANGE_2G | LIS3DHTR_BDU_CONT | LIS3DHTR_HR_DS)
         for i in range(len(numSensors)):
-            if any(addressList):
+            if addressList:
                 bus.write_byte_data(addressList[i], LIS3DHTR_REG_CTRL4, DATA_CONFIG)
 
  
@@ -107,7 +108,7 @@ class LIS3DHTR():
         yAccl = 0
         zAccl = 0
         for i in range(len(self.numSensors)):
-            if any(addressList):
+            if addressList:
                 data0 = bus.read_byte_data(self.addressList[i], LIS3DHTR_REG_OUT_X_L)
                 data1 = bus.read_byte_data(self.addressList[i], LIS3DHTR_REG_OUT_X_H)
  
